d9d37fd67d3d4fa0ba1e9367d8ec494010051889
onSTRANN1Y <splashilia3@gmail.com>
2025-10-26 13:29:45 +0300

    update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,7 +1,7 @@
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-
-
-
+z = 30
+for n in range(30):
+    if n > 10:
+        z = z - n
+    else:
+        z = z + n
+print(z)
